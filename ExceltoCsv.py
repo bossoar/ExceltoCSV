1ea909da14fbfc15bcb3d5d1d84e2aae6a265ab0
bossoar <bossoar@gmail.com>
2022-03-21 13:26:00 -0300

    renombro columnas

--- ExceltoCsv.py
+++ ExceltoCsv.py
@@ -5,18 +5,38 @@ df = pd.read_excel (r'Bajas Tempranas Noviembre 2021 (1).xlsx')
 # remove spaces in columns name
 df.columns = df.columns.str.replace(' ','_')
 
+# str.decode('utf-8').replace(u'\xf1', 'n')
 
-col = df[["Categoría","OS","Mod"]]
-print (col.head())
+df.columns = df.columns.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
 
 
-header = ["Categoría", "OS" , "Mod"]
 
-# Filtro por categoria
-newdf = df[(df.Categoría == "Aplica Baja") ]
+# seleccion de columnas 
+# col = df[["Categoría","OS","Mod","Filial"]]
+print (df.columns)
+
+
+
+
+# Filtro por categoria y filial 
+
+newdf = df.query('Filial in ('"13"', '"9"', '"30"') & Categoria.str.contains("deudor",case=False)')
+print (newdf.columns)
+
+# "Courses in ('Spark','PySpark')"
+
+# Defino las columnas que se van a exportar
+
+
+# # renombro la columna
+newdf = newdf.rename(columns={"MaxDeFIN_REL_LAB":"FECHA"})
+
+header = ["CUIL", "APELLIDO__NOMBRE" , "Razon","FECHA"]
 
 # exporto
-newdf.to_csv('output.csv', columns = header , index=False)
-# & (df.carrier == "Cambio Empleador")]
+
+newdf.to_csv('CartaSocioDeudor.csv', columns = header , index=False)
+# newdf.to_csv('CartaSocioDeudor.csv', index=False)
+# 
 
 print(newdf)
