1ea532dff0e210ce6d804711fc12a5f935480b5e
ARMANDO BOSSO <bossoar@gmail.com>
2022-03-28 16:48:26 -0300

    upload and download file

--- WebMPP.py
+++ WebMPP.py
@@ -1,5 +1,6 @@
 from logging import debug
 from os import P_DETACH
+from warnings import catch_warnings
 from flask import Flask
 from flask import render_template,request,redirect
 from logging import FileHandler,WARNING
@@ -11,6 +12,10 @@ import logging
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+# from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
 
 
 logging.basicConfig(filename='error4.log',level=logging.DEBUG)
@@ -18,32 +23,66 @@ logging.basicConfig(filename='error4.log',level=logging.DEBUG)
 
 app = Flask(__name__)
 
-# Carpeta de subida
-app.config['UPLOAD_FOLDER'] = './Archivos'
 
+# This is the path to the upload directory
+app.config['UPLOAD_FOLDER'] = 'Archivos/'
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','xls','xlsx'])
 
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-@app.route("/")
-def upload_file():
-     # renderiamos la plantilla "formulario.html"
- return render_template('form.html')
-
-@app.route("/upload", methods=['POST'])
-def uploader():
- if request.method == 'POST':
-  # obtenemos el archivo del input "archivo"
-  f = request.files['archivo']
-  filename = secure_filename(f.filename)
-  # Guardamos el archivo en el directorio "Archivos PDF"
-  f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-  # Retornamos una respuesta satisfactoria
-  return "<h1>Archivo subido exitosamente</h1>"
-
-
-
-
+# This route will show a form to perform an AJAX request
+# jQuery is loaded to execute the request and update the
+# value of the operation
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded files
+    uploaded_files = request.files.getlist("file[]")
+    print(uploaded_files)
+    filenames = []
+    for file in uploaded_files:
+        # Check if the file is one of the allowed types/extensions
+        if file and allowed_file(file.filename):
+            # Make the filename safe, remove unsupported chars
+            filename = secure_filename(file.filename)
+            # Move the file form the temporal folder to the upload
+            # folder we setup
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # Save the filename into a list, we'll use it later
+            filenames.append(filename)
+            # Redirect the user to the uploaded_file route, which
+            # will basicaly show on the browser the uploaded file
+    # Load an html page with a link to each uploaded file
+    return render_template('upload.html', filenames=filenames)
+
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+
+
+        
+    
+
+
+
+@app.route('/ExportCSV')
+def ExportCSVex():
                 
             df = pd.read_excel (r'Bajas Tempranas Noviembre 2021.xlsx')
             # ventana = tkinter.Tk()
@@ -121,4 +160,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)            
+    app.run(debug=True) 
