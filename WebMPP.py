b52e7d318bb54e16e6b1c9ebd4334ec7b1ce79b8
bossoar <bossoar@gmail.com>
2022-04-05 14:55:44 -0300

    creo zip

--- WebMPP.py
+++ WebMPP.py
@@ -16,7 +16,8 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 # from werkzeug import secure_filename
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
-
+import zipfile
+import shutil
 
 logging.basicConfig(filename='error4.log',level=logging.DEBUG)
 
@@ -31,14 +32,33 @@ app.config['CSV_FOLDER'] = 'CSV/'
 # These are the extension that we are accepting to be uploaded
 app.config['ALLOWED_EXTENSIONS'] = set(['xls','xlsx','XLS','XLSX'])
 
+# Creo el zip con los csv
+def csv_to_zip():
+
+    try:
+
+        print(os.path.join(app.config['CSV_FOLDER']))
+        print(os.listdir(os.path.join(app.config['CSV_FOLDER'])))
+        
+        list_files = os.listdir(os.path.join(app.config['CSV_FOLDER']))
+        with zipfile.ZipFile('csv.zip','w') as zipF:
+
+            # Add multiple files to the zip
+                    zipF.write(os.path.join(app.config['CSV_FOLDER'])+'CartaAnalisisManual.csv')
+                    zipF.write(os.path.join(app.config['CSV_FOLDER'])+'CartaEmpresa.csv')
+                    zipF.write(os.path.join(app.config['CSV_FOLDER'])+'CartaSocioDeudor.csv')
+                    zipF.write(os.path.join(app.config['CSV_FOLDER'])+'CartaSocioSinDeuda.csv')
+
+    except Exception as e:
+                    print(e) 
+
+
+
 # For a given file, return whether it's an allowed type or not
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-# This route will show a form to perform an AJAX request
-# jQuery is loaded to execute the request and update the
-# value of the operation
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -62,12 +82,11 @@ def upload():
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 # Save the filename into a list, we'll use it later
                 filenames.append(filename)
-                # Redirect the user to the uploaded_file route, which
-                # will basicaly show on the browser the uploaded file
-        # Load an html page with a link to each uploaded file
     
         # uno los archivos y filtro .
         ExportCSV()
+
+        csv_to_zip()
         # print(filenames)
         # return render_template('download.html',filename=filename)
         return render_template('download.html')
@@ -78,18 +97,10 @@ def upload():
 # Para descargar los CSV
 @app.route('/download')
 def download_file():
-	path = app.config['CSV_FOLDER']+"CartaSocioDeudor.csv"
+	path = "csv.zip"
     # print(send_from_directory(app.config['CSV_FOLDER'])
 	return send_file(path, as_attachment=True)    
 
-    # return send_from_directory(app.config['CSV_FOLDER'],
-    #                            filename,as_attachment=True)
-
-
-# This route is expecting a parameter containing the name
-# of a file. Then it will locate that file on the upload
-# directory and show it on the browser, so if the user uploads
-# an image, that image is going to be show after the upload
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['CSV_FOLDER'],
@@ -105,6 +116,7 @@ def ExportCSV():
             i = 1
 
             # recorro la carpeta de los archivos
+            # Verifico si es bajas o patagonia
             with os.scandir(os.path.join(app.config['UPLOAD_FOLDER'])) as ficheros:
                 for fichero in ficheros: 
                     if 'Bajas' in fichero.name  :                  
@@ -114,64 +126,26 @@ def ExportCSV():
                     i += 1
 
 
-            # Verifico si es bajas o patagonia
-            
 
-            print(Bajas[1])    
-            print(Patagonia[2])  
             
             Bajas1 = pd.read_excel(os.path.join(os.path.join(app.config['UPLOAD_FOLDER']), Bajas[1] ))
             # ventana = tkinter.Tk()
 
             Patagonia2 = pd.read_excel(os.path.join(os.path.join(app.config['UPLOAD_FOLDER']), Patagonia[2] ))
 
-            
-
             Bajas1.columns = Bajas1.columns.str.replace(' ','_')
             Patagonia2.columns = Patagonia2.columns.str.replace(' ','_')
-
-            
 
             Bajas1.columns = Bajas1.columns.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
             Patagonia2.columns = Patagonia2.columns.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
 
-            # Filtro por categoria y filial 
-            # newBajas1 = Bajas1.query('Filial in ('"13"', '"9"', '"30"') & Categoria.str.contains("deudor",case=False)')
-            print (Bajas1.columns)
-            print (Patagonia2.columns)
-
-
-            # Filtro por categoria y filial 
-            # newPatagonia2 = Patagonia2.query('Filial in ('"13"', '"9"', '"30"') & Categoria.str.contains("deudor",case=False)')
-            # print (newPatagonia2)
-
-
-            # renombro la columna
-            # newBajas1 = newBajas1.rename(columns={"MaxDeFIN_REL_LAB":"FECHA DE BAJA ABT"})
-
-            # newPatagonia2 = newPatagonia2.rename(columns={"Filial":"Filial"})
-            # newPatagonia2 = newPatagonia2.rename(columns={"CUILSOC":"CUIL"})
-            # newPatagonia2 = newPatagonia2.rename(columns={"RAZON SOCIAL":"Razon"})
-
-            # print(newPatagonia2) 
-
+  
             # Defino las columnas que se van a exportar
             header = ["Categoria", "Mod" ,"CUIL","ICSoc","APELLIDO__NOMBRE","CUIT","Razon","MaxDeFIN_REL_LAB",]
-            # print (newBajas1.columns)
-
-
-            # Combino las columnas de nombre y apellido 
-            # newPatagonia2["APELLIDO__NOMBRE"] = newPatagonia2["APELLIDO"] +" "+ newPatagonia2["NOMBRE"]
-            # print(newPatagonia2)
-
-            # Defino las columnas que se van a exportar
-            # header2 = ["CUIL", "APELLIDO__NOMBRE" , "Razon"]
 
             frames = [Bajas1, Patagonia2]
 
             result = pd.concat(frames)
-
-            # print(frames)
 
             # Filtro por categoria y filial 
             newrCartaSocioDeudor = result.query('Filial in ('"13"', '"9"', '"30"') & Categoria.str.contains("deudor",case=False)')
@@ -181,25 +155,16 @@ def ExportCSV():
             newrCartaSocioSinDeuda = result.query('Filial in ('"13"', '"9"', '"30"') & ~Categoria.str.contains("deudor",case=False) & Categoria.str.contains("CartaSocio",case=False)')
 
             newrAnalisisManual = result.query('Filial in ('"13"', '"9"', '"30"') & ~Categoria.str.contains("deudor",case=False) &  ~Categoria.str.contains("CartaSocio",case=False) & ~Categoria.str.contains("Empresa",case=False)')
-            
-            # print(newrCartaEmpresa)
-            
 
             # exporto
             newrCartaSocioDeudor.to_csv(os.path.join(app.config['CSV_FOLDER'],'CartaSocioDeudor.csv'), columns = header , index=False)
             newrCartaEmpresa.to_csv(os.path.join(app.config['CSV_FOLDER'],'CartaEmpresa.csv'), columns = header , index=False)
             newrCartaSocioSinDeuda.to_csv(os.path.join(app.config['CSV_FOLDER'],'CartaSocioSinDeuda.csv'), columns = header , index=False)
-            newrAnalisisManual.to_csv(os.path.join(app.config['CSV_FOLDER'],'CartaAnalisisManual .csv'), columns = header , index=False)
-
-
-            # print(newPatagonia2)    
-           
+            newrAnalisisManual.to_csv(os.path.join(app.config['CSV_FOLDER'],'CartaAnalisisManual.csv'), columns = header , index=False)
 
             return render_template('upload.html')
         except Exception as e:
                     print(e) 
-
-
 
 
 
